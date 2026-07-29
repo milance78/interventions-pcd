e948e8f9c8c9e5fc74384226d037cd2e1fbaf7fb
Milan <milance78@yahoo.com>
2026-07-29 22:37:45 +0200

    deleted uncecessary files

--- public/interventions-pcd-main/src/pages/statisticsPage/StatisticsPage.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import "./StatisticsPage.scss";
-const StatisticsPage = () => {
-  return <div className="statistics-page">{"Statistics Page"}</div>;
-};
-export default StatisticsPage;
--- public/interventions-pcd-main/src/pages/templatesPage/TemplatesPage.tsx
+++
@@ -1,6 +0,0 @@
-import * as React from "react";
-import "./TemplatesPage.scss";
-const TemplatesPage = () => {
-  return <div className="templates-page">{"Templates Page"}</div>;
-};
-export default TemplatesPage;
