532f16b5d3282be4befeacdbf5818706882be9d4
Milan <milance78@yahoo.com>
2026-08-16 01:51:40 +0200

    Adjusting 33

--- src/assets/svg/Light bulb off.svg.tsx
+++ src/assets/svg/Light bulb off.svg.tsx
@@ -22,7 +22,7 @@ const LightBulbOffIcon = forwardRef<SVGSVGElement, LightBulbOffIconProps>(
       <path
         d=" M256 105 C145 105 82 180 108 280 C125 350 180 375 185 430 C187 455 210 468 256 468 C302 468 325 455 327 430 C332 375 387 350 404 280 C430 180 367 105 256 105Z"
         fill="#F1F3F5"
-        stroke="#B8BEC5"
+        stroke="#AEB5BD"
         strokeWidth="18"
         strokeLinejoin="round"
       />
@@ -34,28 +34,28 @@ const LightBulbOffIcon = forwardRef<SVGSVGElement, LightBulbOffIconProps>(
       />
       <path
         d=" M220 290 C205 265 185 275 190 305 C195 330 225 325 240 290 C255 255 270 255 282 290 C295 325 320 330 322 300 C324 275 300 265 285 290"
-        stroke="#C9CED4"
+        stroke="#AEB5BD"
         strokeWidth="10"
         fill="none"
         strokeLinecap="round"
       />
       <path
         d="M238 300V462M278 300V462"
-        stroke="#C9CED4"
+        stroke="#AEB5BD"
         strokeWidth="10"
         strokeLinecap="round"
       />
       <path
         d=" M195 455 H317 C325 455 330 465 330 480 L322 535 C318 555 295 570 256 570 C217 570 194 555 190 535 L182 480 C182 465 187 455 195 455Z"
-        fill="#E7EAED"
-        stroke="#B8BEC5"
-        strokeWidth="12"
+        fill="#D5DADE"
+        stroke="#9FA8B1"
+        strokeWidth="13"
       />
       <path
         d=" M205 470 C202 500 210 530 238 550 C220 545 210 530 207 510 L198 485 C196 475 200 470 205 470Z"
         fill="#EEEEEE"
       />
-      <g stroke="#CCD2D7" strokeWidth="10" strokeLinecap="round">
+      <g stroke="#9FA8B1" strokeWidth="11" strokeLinecap="round">
         <path d="M188 480H324" />
         <path d="M190 505H322" />
         <path d="M194 530H318" />
